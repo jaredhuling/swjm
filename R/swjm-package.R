d217e55fc9124f2e60b5e033b6ec5e6e60eb0c45
Jared Huling <jaredhuling@gmail.com>
2026-04-07 13:08:58 -0500

    adds in missing docs

--- R/swjm-package.R
+++ R/swjm-package.R
@@ -38,7 +38,7 @@
 #' @useDynLib swjm, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @importFrom Matrix Diagonal
-#' @importFrom stats approx aggregate coef predict
+#' @importFrom stats approx aggregate coef predict sd
 #'   rgamma rnorm rexp rpois runif rweibull
 #' @importFrom graphics par matplot lines abline legend text axis mtext barplot
 #' @importFrom grDevices adjustcolor
